888d6f981262d7e9068284086b41f080fca88716
Shenal Akalanka <shenalakalanka513@gmail.com>
2025-03-21 23:14:27 +0530

    Homepage and API documentation

---
+++ src/components/homeNavbar/HomeNavbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./homeNavbar.scss"
+import { useNavigate } from 'react-router-dom'
+
+const HomeNavbar = () => {
+
+  const navigate = useNavigate();
+  return (
+    <div className='home-navbar-container'>
+      <div className="home-navbar-logo">PortoLab</div>
+      <div className="home-navbar-items">
+        <div className="home-navbar-item" onClick={()=> navigate("/")}>Home</div>
+        <div className="home-navbar-item">About Us</div>
+        <div className="home-navbar-item" onClick={()=> navigate("/api")}>Documentation</div>
+        <div className="home-navbar-item">Contact Us</div>
+      </div>
+      <div className="home-navbar-login">
+        <div className="home-navbar-login-button" onClick={()=> navigate("/login")}>Login</div>
+        <div className="home-navbar-signup-button" onClick={()=> navigate("/register")}>Sign Up</div>
+      </div>
+        </div>
+  )
+}
+
+export default HomeNavbar
